b8cde3036d20763099833326cb9361b567311aa7
pavankumarkilari <pavankumarkilari519@gmail.com>
2023-12-04 19:19:20 +0530

    C# Assignment

---
+++ Dec04/ConAppAS26/ConAppAS26/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Assignmet_Serialization
+{
+    [Serializable]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ Dec04/ConAppAS26/ConAppAS26/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Assignmet_Serialization
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee()
+            {
+                Id = 999,
+                FirstName = "Chris",
+                LastName = "Gayle",
+                Salary = 150000.00
+            };
+
+            //Binary Serialization and Deserialization
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("employee.bin", FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, obj);
+            stream.Close();
+
+            stream = new FileStream("employee.bin",FileMode.Open, FileAccess.Read);
+
+            Employee employee = (Employee)formatter.Deserialize(stream);
+            Console.WriteLine("* Binary Deserialized Employee *");
+            Console.WriteLine(employee.Id);
+            Console.WriteLine(employee.FirstName);
+            Console.WriteLine(employee.LastName);
+            Console.WriteLine(employee.Salary);
+
+            //XML Serialization and Deserialization
+            XmlSerializer serializer = new XmlSerializer(typeof(Employee));
+            using (TextWriter writer = new StreamWriter("employee.Xml"))
+            {
+                serializer.Serialize(writer, obj);
+            }
+
+            using (TextReader reader = new StreamReader("employee.xml"))
+            {
+                Employee deserializedEmployee = (Employee)serializer.Deserialize(reader);
+                Console.WriteLine("\n** XML Deserialized Employee **");
+                Console.WriteLine($"Id:{deserializedEmployee.Id}, FirstName: {deserializedEmployee.FirstName}, LastName: {deserializedEmployee.LastName}, Salary: {deserializedEmployee.Salary}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
